cf3c0cdeaadfee3598e2fe66d18e51c3694f8e78
Hajime Taniguchi <hameji@gmail.com>
2021-12-07 05:00:11 -0500

    RxSwiftを用いてUITextFieldとUILabelをbind

--- project/RxSwift_UITextLabel_UILabel/ViewController.swift
+++ project/RxSwift_UITextLabel_UILabel/ViewController.swift
@@ -6,45 +6,51 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        textField.delegate = self
-        textField.returnKeyType = .done
+        textField.rx.text.bind(to: label.rx.text).disposed(by: disposeBag)
+
+//        textField.delegate = self
+//        textField.returnKeyType = .done
     }
 
 
 }
 
-extension ViewController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        guard let currentText = textField.text else {
-            return true
-        }
-        print("input string:", string, ", at:", range)
-        var newText = currentText
-        if string == "" {
-            // 文字を削除する
-            let start = currentText.index(currentText.startIndex, offsetBy: range.lowerBound)
-            let end = currentText.index(currentText.startIndex, offsetBy: range.upperBound)
-            let removeRange = start..<end
-            newText.removeSubrange(removeRange)
-        } else {
-            // 文字を追加する
-            newText = currentText + string
-        }
-        label.text = newText
-        return true
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-}
+//extension ViewController: UITextFieldDelegate {
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        guard let currentText = textField.text else {
+//            return true
+//        }
+//        print("input string:", string, ", at:", range)
+//        var newText = currentText
+//        if string == "" {
+//            // 文字を削除する
+//            let start = currentText.index(currentText.startIndex, offsetBy: range.lowerBound)
+//            let end = currentText.index(currentText.startIndex, offsetBy: range.upperBound)
+//            let removeRange = start..<end
+//            newText.removeSubrange(removeRange)
+//        } else {
+//            // 文字を追加する
+//            newText = currentText + string
+//        }
+//        label.text = newText
+//        return true
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.resignFirstResponder()
+//        return true
+//    }
+//}
