1fad1453120ad2affe1d24d47f987b1457ffd486
Hajime Taniguchi <hameji@gmail.com>
2021-12-07 05:00:58 -0500

    不要なコードを削除

--- project/RxSwift_UITextLabel_UILabel/ViewController.swift
+++ project/RxSwift_UITextLabel_UILabel/ViewController.swift
@@ -20,37 +20,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textField.rx.text.bind(to: label.rx.text).disposed(by: disposeBag)
-
-//        textField.delegate = self
-//        textField.returnKeyType = .done
     }
 
 
 }
-
-//extension ViewController: UITextFieldDelegate {
-//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//        guard let currentText = textField.text else {
-//            return true
-//        }
-//        print("input string:", string, ", at:", range)
-//        var newText = currentText
-//        if string == "" {
-//            // 文字を削除する
-//            let start = currentText.index(currentText.startIndex, offsetBy: range.lowerBound)
-//            let end = currentText.index(currentText.startIndex, offsetBy: range.upperBound)
-//            let removeRange = start..<end
-//            newText.removeSubrange(removeRange)
-//        } else {
-//            // 文字を追加する
-//            newText = currentText + string
-//        }
-//        label.text = newText
-//        return true
-//    }
-//
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//        return true
-//    }
-//}
