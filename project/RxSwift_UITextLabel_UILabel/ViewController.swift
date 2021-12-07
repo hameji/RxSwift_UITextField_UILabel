4086bd582258571503c9df468123082fc5dd331e
Hajime Taniguchi <hameji@gmail.com>
2021-12-07 04:17:59 -0500

    keyboardのreturnKeyTypeを指定

--- project/RxSwift_UITextLabel_UILabel/ViewController.swift
+++ project/RxSwift_UITextLabel_UILabel/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         textField.delegate = self
+        textField.returnKeyType = .done
     }
 
 
